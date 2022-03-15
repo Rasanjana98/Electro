816b95461e2fffac9bd71740edf8e52610307e23
Rasanjana Kuruppu <68512019+Rasanjana98@users.noreply.github.com>
2022-03-15 15:16:10 +0530

    predictionScreenDesign

--- app/src/main/java/com/sliit/electro/MainActivity.java
+++ app/src/main/java/com/sliit/electro/MainActivity.java
@@ -2,13 +2,27 @@ package com.sliit.electro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button predictButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        predictButton = findViewById(R.id.predict);
+        predictButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent navToPredict = new Intent(MainActivity.this, PredictBill.class);
+                startActivity(navToPredict);
+            }
+        });
     }
 }
--- app/src/main/java/com/sliit/electro/PredictBill.java
+++ app/src/main/java/com/sliit/electro/PredictBill.java
@@ -3,12 +3,38 @@ package com.sliit.electro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class PredictBill extends AppCompatActivity {
+
+    Spinner buildingTypeSpinner;
+    private static final String[] paths = {"One Story", "Two Story"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_predict_bill);
+
+        buildingTypeSpinner  = findViewById(R.id.buildingType);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, getResources()
+                .getStringArray(R.array.building_array));
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        buildingTypeSpinner.setAdapter(adapter);
+        buildingTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> arg0, View arg1,
+                                       int position, long id) {
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+            }
+        });
+
     }
 }
